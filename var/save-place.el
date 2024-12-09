4a78b33266751ba5398b2069e50419edb2ee9445
fabinholima <fabio.delima21@gmail.com>
2024-12-09 19:01:23 -0400

    keybinds

--- var/save-place.el
+++ var/save-place.el
@@ -1,11 +1,11 @@
 ;;; -*- coding: utf-8; mode: lisp-data -*-
-(("/home/fabio/Nextcloud/SlidesQuimica/QO/Polimeros/Polimeros.org" . 405)
+(("/home/fabio/Nextcloud/SlidesQuimica/main.org" . 511)
+ ("/home/fabio/Nextcloud/SlidesQuimica/QO/Polimeros/Polimeros.org" . 491)
  ("/home/fabio/.emacs.d/var/treemacs/persist.org" . 110)
- ("/home/fabio/.config/awesome/rc.lua" . 4289)
- ("/home/fabio/.config/awesome/"
-  (dired-filename . "/home/fabio/.config/awesome/rc.lua"))
+ ("/home/fabio/Nextcloud/AulasQuimica/Fisico_Quimica/Radioatividade/RadioatividadeII.org" . 18510)
+ ("/home/fabio/Nextcloud/AulasQuimica/Quimica_Geral/LeisPonderais/FormulaMinima.org" . 2599)
+ ("/home/fabio/Nextcloud/AulasQuimica/Quimica_Geral/LeisPonderais/LeiLavosier.org" . 4129)
  ("/home/fabio/Nextcloud/AulasQuimica/main.org" . 1282)
- ("/home/fabio/Nextcloud/SlidesQuimica/main.org" . 514)
  ("/home/fabio/Nextcloud/AulasQuimica/"
   (dired-filename . "/home/fabio/Nextcloud/AulasQuimica/main.org"))
  ("/home/fabio/Nextcloud/AulasQuimica/Redacao/mainRedacao.tex" . 142))
